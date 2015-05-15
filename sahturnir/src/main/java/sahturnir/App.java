75f75bbf5c4bba4c25e980660ad36b64a5323060
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2015-05-15 14:49:51 +0200

    ProjekatKonfiguracija

---
+++ sahturnir/src/main/java/sahturnir/App.java
@@ -0,0 +1,27 @@
+package sahturnir;
+import java.sql.*;
+
+public class App {
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost/sahovski_klub_pijun";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection c = DriverManager.getConnection(url, "siuser", "password123");
+			try {
+				Statement st = c.createStatement();
+				ResultSet rs = st.executeQuery("select * from klubovi;");
+				while (rs.next()) {
+					String naziv = rs.getString(5);
+					System.out.println(naziv);
+				}
+			} catch (Exception e) {
+				System.out.println("Greska pri radu sa bazom: "
+						+ e.getMessage());
+			} finally {
+				c.close();
+			}
+		} catch (Exception e) {
+			System.out.println("Greska pri radu sa bazom: " + e.getMessage());
+		}
+	}
+}
