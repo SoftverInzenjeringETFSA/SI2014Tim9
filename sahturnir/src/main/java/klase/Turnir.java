32a465825b0c56e1fde37622a5ed7ed171f1d0b6
AdnanLFC <amehanovic1@etf.unsa.ba>
2015-05-17 20:37:42 +0200

    Package bug resolved #2
    :'(

---
+++ sahturnir/src/main/java/klase/Turnir.java
@@ -0,0 +1,56 @@
+package klase;
+
+import java.util.Date;
+
+public class Turnir implements java.io.Serializable {
+
+	public Turnir() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private long id;
+	private String naziv;
+	private Date datumPocetka;
+	private int trajanje;
+	private String formatTakmicenja;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public Date getDatumPocetka() {
+		return datumPocetka;
+	}
+
+	public void setDatumPocetka(Date datumPocetka) {
+		this.datumPocetka = datumPocetka;
+	}
+
+	public int getTrajanje() {
+		return trajanje;
+	}
+
+	public void setTrajanje(int trajanje) {
+		this.trajanje = trajanje;
+	}
+
+	public String getFormatTakmicenja() {
+		return formatTakmicenja;
+	}
+
+	public void setFormatTakmicenja(String formatTakmicenja) {
+		this.formatTakmicenja = formatTakmicenja;
+	}
+}
---
+++ sahturnir/src/main/java/sahturnir/App.java
@@ -0,0 +1,70 @@
+package sahturnir;
+
+import java.awt.EventQueue;
+import java.util.Scanner;
+
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+
+import utils.HibernateUtil;
+import gui.Prijava;
+import klase.Turnir;
+import klase.Mec;
+import klase.Takmicar;
+import klase.Klub;
+
+import java.util.Date;
+
+public class App {
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		dodajNesto(session);
+		//nadjiNesto(session);
+		session.close();
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Prijava frame = new Prijava();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	
+	private static void dodajNesto(Session session) {
+		Transaction t1 = session.beginTransaction();
+		Takmicar t = new Takmicar();
+		Klub k = new Klub();
+		k.setNaziv("zoooooohoooooo");
+		k.setSjediste("Uzavreli grad");
+		k.setPredsjednik("Kiss Kiss");
+		k.setDatumOsnivanja(new Date());
+		t.setId(1);
+		t.setIme("imendan");
+		t.setPrezime("prezimendan");
+		t.setJmbg("1234517851754");
+		t.setBrojBodova(5);
+		t.setDatumRodjenja(new Date());
+		t.setKategorija("levat");
+	    Turnir turnircic = new Turnir();
+	    turnircic.setId(1);
+		Mec m = new Mec();
+		m.setTurnir(turnircic);
+		m.setRezultat1(1.);
+		m.setRezultat2(0.);
+		m.setTakmicar1(t);
+		m.setTakmicar2(t);
+		Long id = (Long) session.save(m);
+		//Long id1 = (Long) session.save(t);
+		//Long id2 = (Long) session.save(k);
+		System.out.println("Pozz");
+		t1.commit();
+	}
+}
