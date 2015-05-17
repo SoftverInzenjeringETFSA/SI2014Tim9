9ad49d6199d881a14f4dc6278cdbc5cc68a7b208
ElmaKusundzija <ekusundzij1@etf.unsa.ba>
2015-05-17 13:37:28 +0200

    Revert "elma1"
    This reverts commit 82fb56b8ffd09609463df4a957d12c03c020bdd9.

--- sahturnir/src/main/java/sahturnir/App.java
+++ sahturnir/src/main/java/sahturnir/App.java
@@ -6,7 +6,7 @@ public class App {
 		String url = "jdbc:mysql://localhost/sahovski_klub_pijun";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection c = DriverManager.getConnection(url, "root", "turnir");
+			Connection c = DriverManager.getConnection(url, "siuser", "password123");
 			try {
 				Statement st = c.createStatement();
 				ResultSet rs = st.executeQuery("select * from klubovi;");
