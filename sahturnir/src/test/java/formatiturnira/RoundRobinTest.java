94dfe3dbd8a3e4d10cf10ef46b2e641a0455778a
ElmaKusundzija <ekusundzij1@etf.unsa.ba>
2015-05-20 02:15:18 +0200

    roundRobinTest

---
+++ sahturnir/src/test/java/formatiturnira/RoundRobinTest.java
@@ -0,0 +1,14 @@
+package formatiturnira;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoundRobinTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
