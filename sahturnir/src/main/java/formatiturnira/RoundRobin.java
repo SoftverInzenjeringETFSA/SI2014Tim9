c2935085c123d929bf015ceb277a79c97d87922f
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2015-05-20 02:37:37 +0200

    robinHood

--- sahturnir/src/main/java/formatiturnira/RoundRobin.java
+++ sahturnir/src/main/java/formatiturnira/RoundRobin.java
@@ -1,6 +1,7 @@
 package formatiturnira;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import klase.Mec;
@@ -15,32 +16,24 @@ public class RoundRobin { // NIJE TESTIRANO, TREBA PROVJERITI
 	public List<Mec> RoundRobinGenerator(List<Takmicar> takmicari, Turnir t) {
 		List<Mec> mecevi = new ArrayList<Mec>();
 		Mec m = new Mec();
-		
-		/*for(int i = 0; i < takmicari.size() - 1; i = i + 2)
+		List<Takmicar> tmp = new LinkedList<Takmicar>();
+		tmp = takmicari;
+		int half = tmp.size() / 2;
+		int rounds = tmp.size() - 1;
+
+		for(int k = 0; k < rounds; k++)
 		{
-			for (int j = 1; j < takmicari.size(); j = j + 2)
+			for(int i = 0; i < half; i++)
 			{
-				m.setTakmicar1(takmicari.get(i));
-				m.setTakmicar2(takmicari.get(j));
+				m.setTakmicar1(tmp.get(i));
+				m.setTakmicar2(tmp.get(tmp.size()-i-1));
 				mecevi.add(m);
 			}
-		}*/		
-
-	    int rounds = takmicari.size() - 1;
-	    int halfSize = takmicari.size() / 2;
-
-	    for (int round = 0; round < rounds; round++)
-	    {
-	        for (int idx = 1; idx < halfSize; idx++)
-	        {   			
-	            int takmicar1 = (round + idx) % (1+ mecevi.size());
-	            int takmicar2 = (round  + (1+ mecevi.size()) - idx) % (1+ mecevi.size());
-	            m.setTakmicar1(takmicari.get(takmicar1));
-	            m.setTakmicar2(takmicari.get(takmicar2));
-	            mecevi.add(m);
-	        }
-	    }		
+			tmp.add(1, tmp.get(tmp.size()-1));
+			tmp.remove(tmp.size()-1);
+		}
 		return mecevi;
 	}
 }
 
+	
