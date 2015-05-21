f50479c26b71d8e177412c88ecd58bad8a4b5755
ElmaKusundzija <ekusundzij1@etf.unsa.ba>
2015-05-21 19:34:52 +0200

    RoundRobinTest

--- sahturnir/src/main/java/formatiturnira/RoundRobin.java
+++ sahturnir/src/main/java/formatiturnira/RoundRobin.java
@@ -10,18 +10,24 @@ import klase.Turnir;
 
 public class RoundRobin { // NIJE TESTIRANO, TREBA PROVJERITI
 	List<Mec> mecevi;
-	Mec m;
 	List<Takmicar> tmp;
 
 	public RoundRobin(){
-		List<Mec> mecevi = new ArrayList<Mec>();
-		Mec m = new Mec();
-		List<Takmicar> tmp = new LinkedList<Takmicar>();
+		mecevi = new ArrayList<Mec>();
+		tmp = new ArrayList<Takmicar>();
 	}
 
 
-	public List<Mec> RoundRobinGenerator(List<Takmicar> takmicari, Turnir t) {
-		tmp = takmicari;
+	public List<Mec> RoundRobinGenerator(List<Takmicar> takmicari, Turnir t) throws Exception{
+		
+		if(takmicari.isEmpty())throw new Exception("Nema takmicara!");
+		if(t.getId()==0)throw new Exception("Nepostoji turnir!");
+		
+		/*provjera broja takmicara*/
+		int broj=takmicari.size();
+		if(((broj & (broj-1))!=0)||(broj==0)||(broj==1))throw new Exception("Broj takmicara mora biti potencija broja 2!");
+		
+		tmp.addAll(takmicari);
 		int half = tmp.size() / 2;
 		int rounds = tmp.size() - 1;
 
@@ -29,8 +35,12 @@ public class RoundRobin { // NIJE TESTIRANO, TREBA PROVJERITI
 		{
 			for(int i = 0; i < half; i++)
 			{
+				Mec m=new Mec();
 				m.setTakmicar1(tmp.get(i));
 				m.setTakmicar2(tmp.get(tmp.size()-i-1));
+				m.setRezultat1((double)0);
+				m.setRezultat2((double)0);
+				m.setTurnir(t);
 				mecevi.add(m);
 			}
 			tmp.add(1, tmp.get(tmp.size()-1));
--- sahturnir/src/test/java/formatiturnira/RoundRobinTest.java
+++ sahturnir/src/test/java/formatiturnira/RoundRobinTest.java
@@ -2,13 +2,121 @@ package formatiturnira;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import klase.Mec;
+import klase.Takmicar;
+import klase.Turnir;
+
 import org.junit.Test;
 
 public class RoundRobinTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented"); // TODO
+	public void testRoundRobin() {
+		try {
+			RoundRobin r=new RoundRobin();
+			assertTrue(true);
+			} catch (Exception e) {
+			fail("Izuzetak.");
+			}
 	}
+	@Test
+	public void testGenerisiMeceveNemaTakmicara() {
+		try {
+			RoundRobin r=new RoundRobin();
+			List<Takmicar> takmicari=new ArrayList<Takmicar>();
+			Turnir t=new Turnir();
+			t.setId(5);
+			r.RoundRobinGenerator(takmicari, t);
+			fail("Izuzetak!");
+			} catch (Exception e) {
+			assertTrue(true);
+			}
+	}
+	@Test
+	public void testGenerisiMeceveNemaTurnira() {
+		try {
+			RoundRobin s=new RoundRobin();
+			Takmicar elma=new Takmicar();
+			elma.setId(5);
+			List<Takmicar> takmicari=new ArrayList<Takmicar>();
+			takmicari.add(elma);
+			Turnir t=new Turnir();
+			s.RoundRobinGenerator(takmicari, t);
+			fail("Izuzetak!");
+			} catch (Exception e) {
+			assertTrue(true);
+			}
+	}
+	@Test
+	public void testGenerisiMeceveNedovoljanBrojTakmicara() {
+		try {
+			RoundRobin s=new RoundRobin();
+			Takmicar elma=new Takmicar();
+			elma.setId(5);
+			List<Takmicar> takmicari=new ArrayList<Takmicar>();
+			for(int i=0;i<5;i++)
+			{
+			takmicari.add(elma);
+			}
+			Turnir t=new Turnir();
+			t.setId(5);
+			s.RoundRobinGenerator(takmicari, t);
+			fail("Izuzetak!");
+			} catch (Exception e) {
+			assertTrue(true);
+			}
+	}
+	@Test
+	public void testGenerisiMeceveTacanBroj(){
+		try {
+			RoundRobin r=new RoundRobin();
+			Takmicar elma=new Takmicar();
+			elma.setId(5);
+			List<Takmicar> takmicari=new ArrayList<Takmicar>();
+			for(int i=0;i<16;i++)
+			{
+			takmicari.add(elma);
+			}
+			Turnir t=new Turnir();
+			t.setId(5);
+			
+			
+			assertTrue(r.RoundRobinGenerator(takmicari, t).size()==120);
+			} catch (Exception e) {
+			fail(e.getMessage());
+			}
+	}
+	@Test
+	public void testGenerisiMeceveUparivanje() {
+		try {
+			RoundRobin r=new RoundRobin();
+			List<Takmicar> takmicari=new ArrayList<Takmicar>();
+			for(int i=0;i<4;i++)
+			{
+				Takmicar elma=new Takmicar();
+				elma.setId(5-i);
+				if(i%2==0)elma.setBrojBodova(5-i);
+				if(i%2==1)elma.setBrojBodova(5+i);
+				elma.setIme("elma"+String.valueOf(i));
+				takmicari.add(elma);
+			}
+			
+			Turnir t=new Turnir();
+			t.setId(5);
+			List<Mec> mecevi=new ArrayList<Mec>();
+			mecevi=r.RoundRobinGenerator(takmicari, t);
+			assertTrue(mecevi.get(0).getTakmicar1().getIme().equals("elma0"));
+			assertTrue(mecevi.get(0).getTakmicar2().getIme().equals("elma3"));
+			assertTrue(mecevi.get(1).getTakmicar1().getIme().equals("elma1"));
+			assertTrue(mecevi.get(1).getTakmicar2().getIme().equals("elma2"));
+			
+			} catch (Exception e) {
+			fail(e.getMessage());
+			}
+	}
+	
 
 }
