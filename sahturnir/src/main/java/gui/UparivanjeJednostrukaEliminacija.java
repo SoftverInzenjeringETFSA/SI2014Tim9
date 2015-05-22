760d0abb592e4ecb4a7004836a2a57ad928bf3b8
ekurtanovi3 <ekurtanovi3@etf.unsa.ba>
2015-05-22 22:35:40 +0200

    l

--- sahturnir/src/main/java/gui/UparivanjeJednostrukaEliminacija.java
+++ sahturnir/src/main/java/gui/UparivanjeJednostrukaEliminacija.java
@@ -2,10 +2,14 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Toolkit;
 
 public class UparivanjeJednostrukaEliminacija extends JFrame {
 
@@ -31,13 +35,25 @@ public class UparivanjeJednostrukaEliminacija extends JFrame {
 	 * Create the frame.
 	 */
 	public UparivanjeJednostrukaEliminacija() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(UparivanjeJednostrukaEliminacija.class.getResource("/gui/logo.png")));
+		setTitle("\u0160ahovski klub Pijun");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 845, 540);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		
+	    gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 829, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 502, Short.MAX_VALUE)
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
